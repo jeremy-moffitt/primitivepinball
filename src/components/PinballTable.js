cec9dfa85bb702253dcf69542f00eee838f292b9
jmoffitt <jeremy.moffitt@suse.com>
2018-07-10 14:02:03 -0700

    setup basic plunger (does not vary force yet)

--- src/components/PinballTable.js
+++ src/components/PinballTable.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Pinball from './Pinball.js';
 import Obstacle from './Obstacle.js';
-
+import Draggable from 'react-draggable';
 
 class PinballTable extends Component {
 
@@ -26,10 +26,29 @@ class PinballTable extends Component {
         id: 'wall2'
       }
     ]
+    this.state = {
+      plungerKey: 0
+    }
   }
 
-  plungered = () => {
-    this.pinball.current.applyForce(-5, -500);
+  plungered = (event, element) => {
+    //is the ball at the starting point? if not, don't move it from the plunger applying force
+    if(this.pinball.current.isAtOrigin()) {
+      this.pinball.current.applyForce(-65, -200);
+    }
+    //react-draggable will reset a component state if its key changes
+    // this is the quickest way to reset its state without learning
+    // the inards of the draggable-core library
+    this.setState((prevState) => {
+      return {
+        plungerKey: prevState.plungerKey + 1
+      };
+    });
+  }
+
+  //reset the table, reset the ball to origin
+  ballLost = () => {
+    this.pinball.current.resetToOrigin();
   }
 
   render() {
@@ -40,19 +59,29 @@ class PinballTable extends Component {
     let pinballOrigin = {
       x: this.props.width,
       y: this.props.height
-    }
+    };
+
+    let plungerStyle = {
+      "left": (this.props.width - 20) + 'px',
+      "top": (this.props.height - 80) + 'px'
+    };
 
     return (
         <div>
           <div className='pinball-table' style={ tableSize }>
             <Pinball origin={pinballOrigin} tableprops={this.props}
-                     ref={this.pinball} obstacles={this.tableElements} />
+                     ref={this.pinball} obstacles={this.tableElements}
+                     balllost={this.ballLost}/>
           { this.tableElements.map(item =>
               <Obstacle {...item} key={item.id} /> ) }
           </div>
-          <div className='plunger'>
-            <button onClick={this.plungered}>start game</button>
-          </div>
+          <Draggable
+            onStop={this.plungered}
+            axis="y"
+            bounds={{ top: 0, bottom: 80}}
+            key={this.state.plungerKey}>
+            <div className='plunger' style={ plungerStyle }/>
+          </Draggable>
         </div>
     )
   }
